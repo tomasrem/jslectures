6ab470a402dae86199f6eb6193fecfca94304776
Tomáš Remiš <remiss.tomass@gmail.com>
2022-09-14 19:52:27 +0200

    Some css pointers and shit idk

---
+++ dom.js
@@ -0,0 +1,11 @@
+const para = document.querySelector("p"); // we use document each time we want to do something with the web page
+// it scans the document from top to bootom and pick the first one matching
+
+console.log(para);
+
+const error = document.querySelector(".error"); // selecting by class
+console.log(error);
+
+const error_div = document.querySelector("div.error");
+console.log(error_div);
+// go to Elements/copy/selector to get css pointer :D
